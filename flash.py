4cde0a370a4f87f298e2093172784b6978d88081
crsayen <crsayen@gmail.com>
2022-11-30 23:53:13 -0500

    Update layout Wed Nov 30 23:53:13 EST 2022

--- flash.py
+++ flash.py
@@ -13,8 +13,14 @@ elif sys.argv[1] == 'r':
 else:
     print(f'{sys.argv[1]} is not a hand')
     exit(-1)
+    
+v = '-nice_nano_v2-zmk.uf2'
+if len(sys.argv) > 2:
+    if sys.argv[2] == 'oled':
+        v = '\ nice_view_adapter\ nice_view-nice_nano_v2-zmk.uf2'
+    
 
-n = f'corne_{h}-nice_nano_v2-zmk.uf2'
+n = f'corne_{h}{v}'
 dl = '/Users/chris/Downloads'
 files = os.listdir(dl)
 fws = [(int(f.split('-')[1]), f) for f in files if re.search('^firmware-[0-9]+$', f)]
